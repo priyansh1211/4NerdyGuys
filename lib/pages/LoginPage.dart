85ba6049cea496069fe7af44a72894ef7fdd252c
priyansh <priyanshbhatt1112@gmail.com>
2022-06-25 11:19:53 +0530

    Login Page created

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -18,70 +18,73 @@ class _LoginPageState extends State<LoginPage> {
         padding: const EdgeInsets.all(12),
         child: Container(
           color: Colors.grey.shade100,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(13),
-                child: TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
-                    hintText: 'Enter your name',
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(13),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                      hintText: 'Enter your name',
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                    ),
                   ),
                 ),
-              ),
-              OutlinedButton(
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.blue.shade900)),
-                  child: const Text('Log in',
-                      style: TextStyle(color: Colors.white)),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text("Alert Message"),
-                            content: Text(nameController.text),
-                            actions: <Widget>[
-                              new FlatButton(
-                                child: const Text('OK'),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                              )
-                            ],
-                          );
-                        });
-                  }),
-              SizedBox(
-                height: 10,
-              ),
-              Row(children: [
-                Text('Don\'t have an account?',
-                    style: TextStyle(color: Colors.black, fontSize: 20)),
-                InkWell(onTap: (){},
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(color: Colors.blueAccent, fontSize: 20),
-                  ),
-                )
+                OutlinedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.all(Colors.blue.shade900)),
+                    child: const Text('Log in',
+                        style: TextStyle(color: Colors.white)),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Alert Message"),
+                              content: Text(nameController.text),
+                              actions: <Widget>[
+                                new FlatButton(
+                                  child: const Text('OK'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                )
+                              ],
+                            );
+                          });
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(children: [
+                  Text('Don\'t have an account?',
+                      style: TextStyle(color: Colors.black, fontSize: 20)),
+                  InkWell(onTap: (){},
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(color: Colors.blueAccent, fontSize: 20),
+                    ),
+                  )
+                ],
+                    mainAxisAlignment:MainAxisAlignment.center)
               ],
-                  mainAxisAlignment:MainAxisAlignment.center)
-            ],
+            ),
           ),
         ),
       ),
