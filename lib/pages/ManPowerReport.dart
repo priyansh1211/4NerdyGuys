cd903834a35896abe6274f625efae998b2b8e423
priyansh <priyanshbhatt1112@gmail.com>
2022-07-26 23:04:50 +0530

    Welcome page updated

--- lib/pages/ManPowerReport.dart
+++ lib/pages/ManPowerReport.dart
@@ -1,4 +1,6 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/MachineryReport.dart';
 
 class ManPowerReport extends StatefulWidget {
   const ManPowerReport({Key? key}) : super(key: key);
@@ -8,15 +10,65 @@ class ManPowerReport extends StatefulWidget {
 }
 
 class _ManPowerReportState extends State<ManPowerReport> {
+  final List<String> items = [
+    'Add new column',
+  ];
+  final List<String> Agency =[
+    'Steel',
+    'Shuttering',
+    'Concrete',
+    'Brick Masonary',
+  ];
 
-  String? _chosenValue;
+  String? selectedValue;
+
+  List<DropdownMenuItem<String>> _addDividersAfterItems(List<String> items) {
+    List<DropdownMenuItem<String>> menuItems = [];
+    for (var item in items) {
+      menuItems.addAll(
+        [
+          DropdownMenuItem<String>(
+            value: item,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                item,
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+          //If it's last item, we will not add Divider after it.
+          if (item != items.last)
+            const DropdownMenuItem<String>(
+              enabled: false,
+              child: Divider(),
+            ),
+        ],
+      );
+    }
+    return menuItems;
+  }
+
+  List<int> _getDividersIndexes() {
+    List<int> dividersIndexes = [];
+    for (var i = 0; i < (items.length * 2) - 1; i++) {
+      //Dividers indexes will be the odd indexes
+      if (i.isOdd) {
+        dividersIndexes.add(i);
+      }
+    }
+    return dividersIndexes;
+  }
+  //String? _chosenValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffE5E5E5),
+      backgroundColor: const Color(0xffE5E5E5),
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        backgroundColor: Color(0xffE5E5E5),
+        backgroundColor: const Color(0xffE5E5E5),
         title: const Text(
           "Man Power Report:",
           style: TextStyle(color: Colors.black),
@@ -51,83 +103,105 @@ class _ManPowerReportState extends State<ManPowerReport> {
         shrinkWrap: true,
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Container(
-              padding: EdgeInsets.only(left: 20),
+              padding: const EdgeInsets.only(left: 20),
               height: 50,
-              margin: EdgeInsets.only(right: 160),
+              margin: const EdgeInsets.only(right: 160),
               decoration: BoxDecoration(
                 color: Colors.blue.shade100,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child:  TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  suffixIcon: Icon(Icons.keyboard_arrow_down_rounded),
-                  hintText: 'Add Data',
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  isExpanded: true,
+                  hint: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Add Data',
+                    ),
+                  ),
+                  items: _addDividersAfterItems(items),
+                  customItemsIndexes: _getDividersIndexes(),
+                  customItemsHeight: 4,
+                  value: selectedValue,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedValue = value as String;
+                    });
+                  },
+                  buttonHeight: 40,
+                  buttonWidth: 140,
+                  itemHeight: 40,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 8.0),
                 ),
               ),
             ),
           ),
-          SizedBox(height: 10,),
-          Padding(
+          const SizedBox(
+            height: 10,
+          ),
+          const Padding(
             padding: EdgeInsets.only(left: 25),
             child: Text(
               'Agency',
-              style: TextStyle(color: Colors.black,fontSize: 17),
-            ),
-          ),
-          DropdownButton<String>(
-            focusColor:Colors.white,
-            value: _chosenValue,
-            //elevation: 5,
-            style: TextStyle(color: Colors.white),
-            iconEnabledColor:Colors.black,
-            items: <String>[
-              'Android',
-              'IOS',
-              'Flutter',
-              'Node',
-              'Java',
-              'Python',
-              'PHP',
-            ].map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value,style:TextStyle(color:Colors.black),),
-              );
-            }).toList(),
-            hint:Text(
-              "-Select Agency-",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500),
-            ),
-            onChanged: (String? value) {
-              setState(() {
-                _chosenValue = value;
-              });
-            },
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 24),
-            child: Text(
-              'Description',
-              style: TextStyle(color: Colors.black,fontSize: 15),
+              style: TextStyle(color: Colors.black, fontSize: 17),
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Container(
-              padding: EdgeInsets.only(left: 20),
+              padding: const EdgeInsets.only(left: 20),
               height: 50,
-              margin: EdgeInsets.only(right: 100),
+              margin: const EdgeInsets.only(right: 100),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: TextField(
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  isExpanded: true,
+                  hint: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '-Select Agency-',
+                    ),
+                  ),
+                  items: _addDividersAfterItems(Agency),
+                  customItemsIndexes: _getDividersIndexes(),
+                  customItemsHeight: 4,
+                  value: selectedValue,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedValue = value as String;
+                    });
+                  },
+                  buttonHeight: 40,
+                  buttonWidth: 140,
+                  itemHeight: 40,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 24),
+            child: Text(
+              'Description',
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              padding: const EdgeInsets.only(left: 20),
+              height: 50,
+              margin: const EdgeInsets.only(right: 100),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const TextField(
                 decoration: InputDecoration(
                   hintText: '-Give Work Description-',
                   border: InputBorder.none,
@@ -136,24 +210,24 @@ class _ManPowerReportState extends State<ManPowerReport> {
             ),
           ),
           //SizedBox(height: 10,),
-          Padding(
+          const Padding(
             padding: EdgeInsets.only(left: 24),
             child: Text(
               'Skilled',
-              style: TextStyle(color: Colors.black,fontSize: 15),
+              style: TextStyle(color: Colors.black, fontSize: 15),
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Container(
-              padding: EdgeInsets.only(left: 20,top: 5,bottom: 5),
+              padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
               height: 55,
-              margin: EdgeInsets.only(right: 280),
+              margin: const EdgeInsets.only(right: 280),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: TextField(
+              child: const TextField(
                 decoration: InputDecoration(
                   hintText: '1',
                   suffixIcon: Icon(Icons.keyboard_arrow_down_outlined),
@@ -163,24 +237,24 @@ class _ManPowerReportState extends State<ManPowerReport> {
             ),
           ),
           //SizedBox(height: 10,),
-          Padding(
+          const Padding(
             padding: EdgeInsets.only(left: 24),
             child: Text(
               'Unskilled',
-              style: TextStyle(color: Colors.black,fontSize: 15),
+              style: TextStyle(color: Colors.black, fontSize: 15),
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Container(
-              padding: EdgeInsets.only(left: 20,top: 5,bottom: 5),
+              padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
               height: 55,
-              margin: EdgeInsets.only(right: 280),
+              margin: const EdgeInsets.only(right: 280),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child:TextField(
+              child: const TextField(
                 decoration: InputDecoration(
                   hintText: '1',
                   suffixIcon: Icon(Icons.keyboard_arrow_down_outlined),
@@ -190,36 +264,69 @@ class _ManPowerReportState extends State<ManPowerReport> {
             ),
           ),
           //SizedBox(height: 10,),
-          Padding(
+          const Padding(
             padding: EdgeInsets.only(left: 24),
             child: Text(
               'Remarks',
-              style: TextStyle(color: Colors.black,fontSize: 15),
+              style: TextStyle(color: Colors.black, fontSize: 15),
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Container(
+              padding: const EdgeInsets.only(left: 20),
               height: 50,
-              margin: EdgeInsets.only(right: 100),
+              margin: const EdgeInsets.only(right: 100),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
               ),
+              child: const TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
             ),
           ),
-          SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           MaterialButton(
-            onPressed: () {},
+            onPressed: () {
+              AlertDialog(
+                title: const Text('Do you want to continue?'),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MachineryReport()));
+                      },
+                      child: const Text(
+                        'Yes',
+                        style: TextStyle(
+                            color: Color(0xff685D5D), fontFamily: 'ReadexPro'),
+                      ),
+                  ),
+                  ElevatedButton(onPressed: (){
+                   // Navigator.push(context, MaterialPageRoute(builder: (context)=>))
+                  }, child: const Text(
+                    'Yes',
+                    style: TextStyle(
+                        color: Color(0xff685D5D), fontFamily: 'ReadexPro'),
+                  ),
+                  ),
+                ],
+              );
+            },
             child: Container(
               height: 52,
               width: 130,
-              margin: EdgeInsets.symmetric(horizontal: 105),
+              margin: const EdgeInsets.symmetric(horizontal: 105),
               decoration: BoxDecoration(
                 color: Colors.blue.shade600,
                 borderRadius: BorderRadius.circular(35.0),
               ),
-              child: Center(
+              child: const Center(
                 child: Text(
                   "Make Report",
                   style: TextStyle(
