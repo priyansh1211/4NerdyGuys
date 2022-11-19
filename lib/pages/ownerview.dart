ca6de72aa1eceda3ed9e436e61a20d4598540801
sktarvani <tarvanisaket563@gmail.com>
2022-11-19 18:37:11 +0530

    uploaded again

--- lib/pages/ownerview.dart
+++ lib/pages/ownerview.dart
@@ -1,283 +1,131 @@
 import 'package:flutter/material.dart';
-// block function to add
+
 class OwnerPage extends StatefulWidget{
   const OwnerPage ({Key? key}) : super(key:key);
   @override
   State<OwnerPage> createState() => _OwnerPageState();
 }
+
 class _OwnerPageState extends State<OwnerPage>{
+
+  List Listfordate = [
+    '18 DEC 2019',
+    'date 2',
+    'date 3',
+    'date 4',
+    'date 5',
+  ];
+  List Listforname = [
+    'name 1',
+    'name 2',
+    'name 3',
+    'name 4',
+    'name 5',
+  ];
+  List Listforsite = [
+    'site 1',
+    'site 2',
+    'site 3',
+    'site 4',
+    'site 5',
+  ];
+  List ListforBlock = [
+    'block B',
+    'block A',
+    'block C',
+    'block D',
+    'block E',
+  ];
+
   @override
-  Widget build(BuildContext contet){
+  Widget build(BuildContext context){
     var screensize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: Color(0xffE5E5E5),
+      body: Container(
+        color: const Color(0xffE5E5E5),
+        child: Stack (children: [
+          Container(
+            decoration: const BoxDecoration(color: Color(0xffE5E5E5)),
+          ),
+          Positioned(
+            left: screensize.width * 0.05,
+            top: screensize.height * 0.05,
+            child: InkWell(
+              child: const Icon(
+                Icons.arrow_back_ios,
+                size: 30,
               ),
-            ),
-            Positioned(
-              //left: 30,
-              left: screensize.width * 0.08,
-              top: screensize.height * 0.08,
-              child: InkWell(
-                child: const Icon(
-                  Icons.arrow_back_ios,
-                  size: 30,
+              onTap: () {Navigator.of(context).pop();},
+            ),),
+          Positioned(
+            right: screensize.width * 0.05,
+            top: screensize.height * 0.05,
+            child: InkWell(
+              child: const Icon(
+                Icons.calendar_today,
+                size: 30,
+              ),
+              onTap: () {print("Calender pressed");},
+            ),),
+          ListView.builder(
+            itemCount: Listfordate.length,
+            itemBuilder: (BuildContext context, int index){
+              return Card(
+                margin: EdgeInsets.only(
+                  top: screensize.width * 0.03,
+                  left: screensize.width * 0.04,
+                  right: screensize.width * 0.04,
                 ),
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ),
-            Positioned(
-              left: screensize.width * 0.85,
-              top: screensize.height * 0.08,
-              child: InkWell(
-                child: Row(
-                  children: [
-                    Container(
-                      child: const Icon(Icons.calendar_today, size: 28),
-                    ),
-                  ],
+                color:  Color(0xff68B1D5),
+                child: ListTile(
+                  title: Text(Listfordate[index],
+                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,fontFamily: 'Readxpro')),
+                  subtitle: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(top: screensize.height * 0.02),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Text(Listforname[index],
+                            style: TextStyle(fontSize: 18),),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(top: screensize.height * 0.02,),
+                              child: Align(alignment:Alignment.bottomLeft,
+                                  child: Text(Listforsite[index],style:TextStyle(fontSize: 18))),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(top: screensize.height * 0.02,left: screensize.width*0.2),
+                              child: Text(ListforBlock[index],style: TextStyle(fontSize: 18),/*textAlign: TextAlign.center,*/),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: ButtonTheme(
+                          child: ButtonBar(
+                            children: <Widget>[
+                              ElevatedButton(
+                                child: Text("View Report"),
+                                onPressed: () {print("Go to view page");},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              width: screensize.width * 0.91,
-              height: screensize.height * 0.19,
-              decoration: const BoxDecoration(
-                color:  Color(0xffA6D1E6),
-              ),
-              margin: EdgeInsets.only(
-                top: screensize.height * 0.15,
-                left: screensize.width * 0.04,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.02,
-                      right: screensize.width * 0.6,
-                    ),
-                    child: Text(
-                      '18 Dec 2019',
-                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                    ),
-                    //Positioned(),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.01,
-                      right: screensize.width * 0.55,
-                    ),
-                    child: Text(
-                      'Vishwas Bhatt',
-                      style: TextStyle(fontSize: 20, fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.01,
-                      right: screensize.width * 0.67,
-                    ),
-                    child: Text(
-                      'Sanskruti',
-                      style: TextStyle(fontSize: 18,fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.0002,
-                      left: screensize.width * 0.6,
-                    ),
-                    child: TextButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(255, 240, 255, 100))
-                      ),
-                      onPressed: () {},
-                      child: Text('View Report'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: screensize.width * 0.91,
-              height: screensize.height * 0.19,
-              decoration: const BoxDecoration(
-                color: Color(0xffA6D1E6),
-              ),
-              margin: EdgeInsets.only(
-                top: screensize.height * 0.35,
-                left: screensize.width * 0.04,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.02,
-                      right: screensize.width * 0.6,
-                    ),
-                    child: Text(
-                      '18 Dec 2019',
-                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                        top: screensize.height * 0.01,
-                        right: screensize.width * 0.55),
-                    child: Text(
-                      'Priyansh Bhatt',
-                      style: TextStyle(fontSize: 20, fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.01,
-                      right: screensize.width * 0.69,
-                    ),
-                    child: Text(
-                      'Sanskruti',
-                      style: TextStyle(fontSize: 18,fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.0002,
-                      left: screensize.width * 0.6,
-                    ),
-                    child: TextButton(
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(255, 240, 255, 100))
-                      ),
-                      onPressed: () {},
-                      child: Text('View Report'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: screensize.width * 0.91,
-              height: screensize.height * 0.19,
-              decoration: const BoxDecoration(
-                color: Color(0xffA6D1E6),
-              ),
-              margin: EdgeInsets.only(
-                top: screensize.height * 0.55,
-                left: screensize.width * 0.04,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.02,
-                      right: screensize.width * 0.6,
-                    ),
-                    child: Text(
-                      '18 Dec 2019',
-                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                        top: screensize.height * 0.01,
-                        right: screensize.width * 0.50),
-                    child: Text(
-                      'Shivam Shethiya',
-                      style: TextStyle(fontSize: 20, fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.01,
-                      right: screensize.width * 0.69,
-                    ),
-                    child: Text(
-                      'Sanskruti',
-                      style: TextStyle(fontSize: 18,fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.0002,
-                      left: screensize.width * 0.6,
-                    ),
-                    child: TextButton(
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(255, 240, 255, 100))
-                      ),
-                      onPressed: () {},
-                      child: Text('View Report'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: screensize.width * 0.91,
-              height: screensize.height * 0.19,
-              decoration: const BoxDecoration(
-                color: Color(0xffA6D1E6),
-              ),
-              margin: EdgeInsets.only(
-                top: screensize.height * 0.75,
-                left: screensize.width * 0.04,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.02,
-                      right: screensize.width * 0.6,
-                    ),
-                    child: Text(
-                      '18 Dec 2019',
-                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.01,
-                      right: screensize.width * 0.55,
-                    ),
-                    child: Text(
-                      'Vedant Chavan',
-                      style: TextStyle(fontSize: 20, fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.01,
-                      right: screensize.width * 0.69,
-                    ),
-                    child: Text(
-                      'Sanskruti',
-                      style: TextStyle(fontSize: 18,fontFamily: 'Readxpro'),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: screensize.height * 0.0002,
-                      left: screensize.width * 0.6,
-                    ),
-                    child: TextButton(
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(255, 240, 255, 100))
-                      ),
-                      onPressed: () {},
-                      child: Text('View Report'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+              );
+            },
+          ),
+        ]),
       ),
     );
   }
